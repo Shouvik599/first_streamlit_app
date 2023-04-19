e01a18b80e4bacf38ae890ee805141d30550ceaa
Shouvik Choudhury <50528254+Shouvik599@users.noreply.github.com>
2023-04-19 13:15:05 +0530

    Update streamlit_app.py
    Converted json response to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("FruityVice Fruit Advice")
 fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#normalize json to dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#display the dataframe
+streamlit.dataframe(fruityvice_normalized)
