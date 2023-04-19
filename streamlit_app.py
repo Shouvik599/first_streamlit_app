829b497ece44233ee9da2d3bf7f7de2576f95d5b
Shouvik Choudhury <50528254+Shouvik599@users.noreply.github.com>
2023-04-19 15:02:46 +0530

    Update streamlit_app.py
    bug fix-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -21,6 +22,7 @@ fruityvice_response= requests.get("https://fruityvice.com/api/fruit/"+fruit_choi
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display the dataframe
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
